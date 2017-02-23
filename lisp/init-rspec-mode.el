6888187da01a79f7a55f988992932f9a87804637
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2017-02-23 14:41:50 +1300

    Add powershop-rspec-rerun function

--- lisp/init-rspec-mode.el
+++ lisp/init-rspec-mode.el
@@ -8,14 +8,16 @@
 (setq rspec-primary-source-dirs '("app" "lib" "core" "psnz" "psau" "psuk"))
 (setq rspec-command-options "--color")
 
-
-;;; TODO: make the following work for other spec commands:
-
-;;; TODO: only redefine this key if we're in the Powershop repo:
 (define-key rspec-verifiable-mode-keymap (kbd "v") 'powershop-rspec-verify)
+(define-key rspec-verifiable-mode-keymap (kbd "s") 'powershop-rspec-verify-single)
+(define-key rspec-verifiable-mode-keymap (kbd "r") 'powershop-rspec-rerun)
 
 (defconst powershop-markets '("nz" "au" "uk"))
 (defvar powershop-rspec-verify-history nil)
+
+(defun find-file-in-home ()
+  (interactive)
+  (ido-find-file-in-dir "~/powershop"))
 
 (defun powershop-rspec-verify (market)
   "Run spec for the current buffer in the specified market."
@@ -29,10 +31,29 @@
          (format "PS_MARKET=%s bundle exec spring rspec" market)))
     (rspec-verify)))
 
-;; (defun powershop-migrate-all ()
-;;   "Migrate all powershop market development databases"
-;;   (interactive)
-;;   (shell-command "nmigrate && amigrate && umigrate&"))
+(defun powershop-rspec-verify-single (market)
+  "Run spec for the current example in the specified market."
+  (interactive
+   (list (completing-read "Market: " powershop-markets nil t
+                          nil
+                          'powershop-rspec-verify-history
+                          (or (car powershop-rspec-verify-history) "uk")
+                          )))
+  (let ((rspec-spec-command
+         (format "PS_MARKET=%s bundle exec spring rspec" market)))
+    (rspec-verify-single)))
+
+(defun powershop-rspec-rerun (market)
+  "Re-run the last RSpec invocation in the specified market."
+  (interactive
+   (list (completing-read "Market: " powershop-markets nil t
+                          nil
+                          'powershop-rspec-verify-history
+                          (or (car powershop-rspec-verify-history) "uk")
+                          )))
+  (let ((rspec-spec-command
+         (format "PS_MARKET=%s bundle exec spring rspec" market)))
+    (rspec-rerun)))
 
 
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
