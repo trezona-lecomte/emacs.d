191164c63e48dab7a6c5b39ab96ce3363d8c7ac0
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2017-02-24 10:02:20 +1300

    Ignore the bell ring

--- lisp/init-preferences.el
+++ lisp/init-preferences.el
@@ -1,5 +1,7 @@
 ;;; General preferences that I want to persist so don't do through the customize interface
 
 (setq truncate-lines t)
+(setq ring-bell-function 'ignore)
+
 
 (provide 'init-preferences)
