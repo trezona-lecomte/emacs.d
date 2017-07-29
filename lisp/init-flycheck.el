b6aa2e2d3cf911c6c5be960e0fa0911fb76cba53
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2017-07-29 16:17:50 +1200

    Only check syntax when a buffer is saved or the mode is enabled

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,6 +1,7 @@
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
 
   (when (maybe-require-package 'flycheck-color-mode-line)
     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
