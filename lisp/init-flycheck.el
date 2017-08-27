4e0f79ddec17045703bb06787c20a1809e9c2fdc
Kieran Trezona-le Comte <kieran@fluxfederation.com>
2017-08-27 14:24:34 +1200

    Bind C-c b to flycheck-buffer

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,7 +1,11 @@
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+
+  ;; Tell flycheck when to check syntax automatically:
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
+
+  (define-key global-map (kbd "C-c b") 'flycheck-buffer)
 
   (when (maybe-require-package 'flycheck-color-mode-line)
     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
