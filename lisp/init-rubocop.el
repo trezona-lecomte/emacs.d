1c13755f07b24e07b7c8299f991252f001af2118
Kieran Trezona-le Comte <kieran@fluxfederation.com>
2017-09-27 21:58:22 +1300

    Remove ktlc/rubocop-working-directory

--- lisp/init-rubocop.el
+++ lisp/init-rubocop.el
@@ -2,44 +2,6 @@
 (require-package 'flycheck)
 (require 'flycheck)
 
-(defun ktlc/rubocop-working-directory (arg)
-  "Return the best working directory from which to run rubocop."
-  (or (locate-dominating-file default-directory "Gemfile")
-      default-directory))
-
-(flycheck-define-checker ruby-rubocop
-  "A Ruby syntax and style checker using the RuboCop tool.
-
-You need at least RuboCop 0.34 for this syntax checker.
-
-See URL `http://batsov.com/rubocop/'."
-  :command ("rubocop" "--display-cop-names" "--format" "emacs"
-            ;; Explicitly disable caching to prevent Rubocop 0.35.1 and earlier
-            ;; from caching standard input.  Later versions of Rubocop
-            ;; automatically disable caching with --stdin, see
-            ;; https://github.com/flycheck/flycheck/issues/844 and
-            ;; https://github.com/bbatsov/rubocop/issues/2576
-            "--cache" "false"
-            (config-file "--config" flycheck-rubocoprc)
-            (option-flag "--lint" flycheck-rubocop-lint-only)
-            ;; Rubocop takes the original file name as argument when reading
-            ;; from standard input
-            "--stdin" source-original)
-  :standard-input t
-  :error-patterns
-  ((info line-start (file-name) ":" line ":" column ": C: "
-         (optional (id (one-or-more (not (any ":")))) ": ") (message) line-end)
-   (warning line-start (file-name) ":" line ":" column ": W: "
-            (optional (id (one-or-more (not (any ":")))) ": ") (message)
-            line-end)
-   (error line-start (file-name) ":" line ":" column ": " (or "E" "F") ": "
-          (optional (id (one-or-more (not (any ":")))) ": ") (message)
-          line-end))
-  :working-directory ktlc/rubocop-working-directory
-  :modes (enh-ruby-mode ruby-mode)
-  :next-checkers ((warning . ruby-reek)
-                  (warning . ruby-rubylint)))
-
 
 (provide 'init-rubocop)
 ;;; init-rubocop ends here
