fcf392cfb4fb2b0a7a2040861e0ce6f8c36bf257
Kieran Trezona-le Comte <kieran@fluxfederation.com>
2017-09-28 19:19:52 +1300

    Turn off centered-cursor-mode, as it has odd behaviour with various goto commands

--- lisp/init-center-cursor.el
+++ lisp/init-center-cursor.el
@@ -6,12 +6,12 @@
 ;; Also disable in Info mode, because it breaks going back with the
 ;; backspace key.
 
-(define-global-minor-mode global-centered-cursor-mode centered-cursor-mode
-  (lambda ()
-    (when (not (memq major-mode
-                     (list 'Info-mode 'term-mode 'eshell-mode 'shell-mode 'erc-mode)))
-      (centered-cursor-mode))))
+;; (define-global-minor-mode global-centered-cursor-mode centered-cursor-mode
+;;   (lambda ()
+;;     (when (not (memq major-mode
+;;                      (list 'Info-mode 'term-mode 'eshell-mode 'shell-mode 'erc-mode)))
+;;       (centered-cursor-mode))))
 
-(global-centered-cursor-mode 1)
+;; (global-centered-cursor-mode 1)
 
 (provide 'init-center-cursor)
