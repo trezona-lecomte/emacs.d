6858b2b2edc6afb5e7ef4bf66f6f089fa1347319
Kieran Trezona-le Comte <kieran@fluxfederation.com>
2018-01-12 13:56:04 +1300

    Don't use dynamic-spaces
    I encountered a few instances where this behaviour was annoying, so turn it off.

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -38,9 +38,6 @@
 (add-hook 'after-init-hook 'transient-mark-mode)
 
 
-(when (maybe-require-package 'dynamic-spaces)
-  (dynamic-spaces-global-mode))
-
  ;;; A simple visible bell which works in all terminal types
 
 (defun sanityinc/flash-mode-line ()
